bddc79bc810831791d623f12553a51f0d5008f7e
Falk Kühnel <falk@kuehnel.eu>
2023-12-07 10:50:29 +0100

    Day 05 - ignore test for later
    The PlanterPart2 class has been extracted from Planter. This new class extends Planter and contains the methods for seed extraction and testing. Also, the PlanterMaps array was made protected in the parent class, Planter. A new method, breakPoints, was added to PlanterMap.ts.

---
+++ 05/05-try2.test.ts
@@ -0,0 +1,21 @@
+import {describe, expect, test} from "bun:test";
+import {PlanterMap} from "./PlanterMap.ts";
+import {Planter} from "./Planter.ts";
+import {PlanterPart2} from "./PlanterPart2.ts";
+import sampledata from "./sampleData.txt"
+
+class Breakpoints {
+    private map: Map<number, number>;
+    constructor() {
+        this.map = new Map<number, number>()
+    }
+} 
+describe("05-part2-try 2", () => {
+    test.skip('thing', () => {
+        //let sut = new PlanterPart2("seeds: 2\nseed-to-soil map:\n10 20 2\n30 40 1");
+        let sut = new PlanterPart2(sampledata);
+
+       expect(sut.testing(14, "temperature")).toBeTrue()
+    })
+
+})
