41d4495f9b32c3f277e408e1a763ece100bcb0e6
Falk Kühnel <falk@kuehnel.eu>
2023-12-07 17:48:14 +0100

    Day 07 - Part 1

---
+++ 07/07.test.ts
@@ -0,0 +1,157 @@
+import {describe, expect, test} from "bun:test";
+import {CamelGame, sort} from "./camelGame.ts";
+import testdata from "./testdata.txt";
+import redditdata from "./redditdata.txt";
+describe("Day 07", () => {
+
+    let highCard_5 = "12345"
+    let highCard_6 = "12346"
+    let highCard_2nd_low = "12A46"
+    let highCard_2nd_high = "1K346"
+    let pair     = "11345"
+    let pair_unordered = "13451"
+    let pair_high = "61346"
+    let three_of_a_kind_6 = "61646"
+    test("high card", () => {
+        expect(sort([highCard_5,highCard_6])).toEqual([highCard_5,highCard_6])
+    })
+
+    test("high card change", () => {
+        expect(sort([highCard_6,highCard_5])).toEqual([highCard_5,highCard_6])
+    })
+
+    test("high card change", () => {
+        expect(sort([highCard_2nd_low,highCard_5])).toEqual([highCard_5,highCard_2nd_low])
+    })
+
+    test("high card change", () => {
+        expect(sort([highCard_2nd_low,highCard_2nd_high])).toEqual([highCard_2nd_low, highCard_2nd_high])
+    })
+
+    test("three of a kind", () => {
+        expect(sort([three_of_a_kind_6,highCard_2nd_high])).toEqual([highCard_2nd_high,three_of_a_kind_6])
+    })
+
+
+
+    test.each([
+        [[highCard_5, pair],[highCard_5, pair]],
+        [[pair, highCard_5],[highCard_5, pair]],
+        [[pair_unordered, highCard_5],[highCard_5, pair_unordered]],
+        [[highCard_6, pair_unordered],[highCard_6, pair_unordered]],
+        [[pair_high, pair_unordered],[pair_unordered, pair_high]]
+    ])("pairs: %o to %o", (actual, expected) => {
+        expect(sort(actual)).toEqual(expected)
+    })
+
+
+
+    test("same pair, different first card", () => {
+        let low = "1K2K3"
+        let high ="TK2K3"
+        expect(sort([high,low])).toEqual([low,high])
+    })
+
+    test("two pairs", () => {
+        let high = "2626K"
+        let low ="TK2K3"
+        expect(sort([high,low])).toEqual([low,high])
+    })
+
+    test("full house", () => {
+        let high = "26262"
+        let low ="KKKT2"
+        expect(sort([high,low])).toEqual([low,high])
+    })
+
+    test("four of a kind", () => {
+        let high = "22262"
+        let low ="KKKTT"
+        expect(sort([high,low])).toEqual([low,high])
+    })
+
+    test("five of a kind", () => {
+        let high = "22222"
+        let low ="KKKKT"
+        expect(sort([high,low])).toEqual([low,high])
+    })
+
+    test("game", () => {
+        let gameData = "32T3K 765";
+        let game = new CamelGame(gameData)
+        expect(game.value()).toBe(765)
+    })
+
+    test("2 hands", () => {
+        let gameData = "32T3K 11\n12345 10";
+        let game = new CamelGame(gameData)
+        expect(game.value()).toBe(32)
+    })
+
+    test("2 hands", () => {
+        let gameData = "28547 11\n62347 10";
+        let game = new CamelGame(gameData)
+        expect(game.value()).toBe(31)
+    })
+
+    test.each(
+        [
+            ["five kind, 2nd rule", "KKKKK 1\nAAAAA 2", 5],
+            ["five kind, four of a kind", "KKKKK 2\nKAAAA 1", 5],
+            ["five kind, three of a kind", "KKKKK 2\nKTAAA 1", 5],
+            ["five kind, 2 pairs", "KKKKK 2\nKTATA 1", 5],
+            ["five kind, 1 pairs", "KKKKK 2\nKT5TA 1", 5],
+            ["five kind, nothing", "KKKKK 2\n23456 1", 5],
+            ["four kind, 2nd rule", "AKKKK 2\nKAAAA 1", 5],
+            ["four kind, three of a kind", "AKKKK 2\nKTAAA 1", 5],
+            ["four kind, 2 pairs", "AKKKK 2\nKTATA 1", 5],
+            ["four kind, 1 pairs", "AKKKK 2\nKTA5A 1", 5],
+            ["four kind, nothing", "AKKKK 2\n23456 1", 5],
+            ["three kind, 2nd rule", "A3KKK 2\n9KK5K 1", 5],
+            ["three kind, 2 pairs", "A3KKK 2\n99K5K 1", 5],
+            ["three kind, 1 pairs", "A3KKK 2\n98K5K 1", 5],
+            ["three kind, nothing", "A3KKK 2\n65432 1", 5],
+            ["2 pairs, 2nd rule", "KTKT3 2\nKTKT2 1", 5],
+            ["2 pairs, 1 pair", "KTKT3 2\nKT8T2 1", 5],
+            ["2 pairs, nothing", "KTKT3 2\nAKQJT 1", 5],
+            ["1 pairs, 2nd rule", "K8KT3 2\n8KKT3 1", 5],
+            ["1 pairs, nothing", "K8KT3 2\nT9876 1", 5],
+            ["nothing, 2nd rule", "A2345 2\nK2345 1", 5],
+            ["nothing, 2nd rule", "A2345 2\nQ2345 1", 5],
+        ]
+    )("2 hands: %s", (title, gameData: string, expected: number) => {
+        let game = new CamelGame(gameData)
+        expect(game.value()).toBe(expected)
+    })
+
+    test.each(
+        [
+            ["five kind, 2nd rule", "AAAAA 3\nKKKKK 2\n67QTK 1", 14],
+            ["five kind, 2nd rule", "AA7AA 3\nAA8AA 2\n67QTK 1", 13],
+        ]
+    )("3 hands: %s", (title, gameData: string, expected: number) => {
+        let game = new CamelGame(gameData)
+        expect(game.value()).toBe(expected)
+    })
+
+    test("demo test", () => {
+        let gameData = "32T3K 765\n" +
+            "T55J5 684\n" +
+            "KK677 28\n" +
+            "KTJJT 220\n" +
+            "QQQJA 483";
+        let game = new CamelGame(gameData)
+        expect(game.value()).toBe(6440)
+    })
+
+    test("reddit test", () => {
+        let game = new CamelGame(redditdata)
+        expect(game.value()).toBe(6592)
+    })
+
+    test("final test", () => {
+        let game = new CamelGame(testdata)
+        expect(game.value()).toBe(249748283)
+    })
+
+})
---
+++ 07/camelGame.ts
@@ -0,0 +1,182 @@
+let cardMap: { [key: string]: number } = {
+    "A": 14,
+    "K": 13,
+    "Q": 12,
+    "J": 11,
+    "T": 10,
+    "9": 9,
+    "8": 8,
+    "7": 7,
+    "6": 6,
+    "5": 5,
+    "4": 4,
+    "3": 3,
+    "2": 2,
+    "1": 1
+};
+
+function maxCardValue() {
+    return (max: number, card: string) => {
+        return Math.max(max, cardMap[card])
+    };
+}
+function compareHighCard(handA: string, handB: string): number {
+
+    let maxA = handA.split("").reduce(maxCardValue(), 0)
+    let maxB = handB.split("").reduce(maxCardValue(), 0)
+    return maxA - maxB
+}
+
+function compareStrongerFirstCard(a: string, b: string): number {
+    for (let i = 0; i < 5; i++) {
+        if (cardMap[a[i]] > cardMap[b[i]]) {
+            return 1
+        }
+        if (cardMap[a[i]] < cardMap[b[i]]) {
+            return -1
+        }
+    }
+    return 0
+}
+
+function cardCount(input: string) {
+    let map: Map<string, number> = new Map<string, number>()
+    for (let i = 0; i < input.length; i++) {
+        if (map.has(input[i])) {
+            map.set(input[i], map.get(input[i]) + 1)
+        } else {
+            map.set(input[i], 1)
+        }
+    }
+    return Array.from(map.values()).sort().reverse();
+}
+
+function has2Pairs(input: string) {
+    let count = cardCount(input);
+    return count.reduce((numberOfPairs, amount) => {
+        if (amount == 2) {
+            return numberOfPairs + 1
+        }
+        return numberOfPairs
+    }, 0) == 2
+}
+
+function isPair(input: string) {
+    let count = cardCount(input);
+    return count.indexOf(2) > -1 && count.length == 4
+}
+
+function isHighCard(input: string) {
+    let count = cardCount(input);
+    return count.length == 5
+}
+
+
+function hasThreeOfAKind(a: string) {
+    let count = cardCount(a);
+    return count.indexOf(3) > -1
+}
+
+function hasFourOfAKind(a: string) {
+    let count = cardCount(a);
+    return count.indexOf(4) > -1
+}
+function hasFiveOfAKind(a: string) {
+    let count = cardCount(a);
+    return count.indexOf(5) > -1
+}
+
+function hasFullHouse(input: string) {
+    let count = cardCount(input);
+    return count.indexOf(2) > -1 && count.indexOf(3) > -1
+}
+
+function has(a: string, b: string, hasFunction: (a: string) => Boolean) {
+    let result = undefined
+    let hasA = hasFunction(a)
+    let hasB = hasFunction(b)
+    if (hasA && !hasB) {
+        result = 1
+    } else if (!hasA && hasB) {
+        result = -1
+    } else if (hasA && hasB) {
+        result = compareStrongerFirstCard(a, b)
+    }
+    return result;
+}
+
+
+
+export function sort(hands: string[]) {
+    hands = hands.sort((a, b) => {
+        let result
+        let x = [
+            hasFiveOfAKind,
+            hasFourOfAKind,
+            hasFullHouse,
+            hasThreeOfAKind,
+            has2Pairs,
+            isPair,
+            isHighCard
+        ]
+        for (const hasFn of x) {
+            result = has(a, b, hasFn);
+            if (result !== undefined) {
+                return result
+            }
+        }
+        return result
+    })
+    return hands;
+}
+
+function typeofwin(hand: string) {
+    let result = ""
+    if (hasFiveOfAKind(hand)) {
+        result += " - 5 of a kind"
+    }
+    if (hasFourOfAKind(hand)) {
+        result += " - 4 of a kind"
+    }
+    if (hasFullHouse(hand)) {
+        result += " - full house"
+    }
+    if (hasThreeOfAKind(hand)) {
+        result += " - 3 of a kind"
+    }
+    if (has2Pairs(hand)) {
+        result += " - 2 pairs"
+    }
+    if (isPair(hand)) {
+        result += " - pair"
+    }
+    return result
+}
+
+export class CamelGame {
+    private hands: Map<string, number> = new Map<string, number>();
+
+    constructor(gameData: string) {
+        let lines = gameData.split("\n")
+        lines.forEach(line => {
+            let [hand, bet] = line.split(" ")
+            if (this.hands.has(hand)) {
+                console.log("Duplicate hand: " + hand, bet)
+            }
+            this.hands.set(hand, Number(bet))
+        })
+
+    }
+
+    value() {
+        let hands = sort(Array.from(this.hands.keys()))
+        let sum: number = 0
+        hands.forEach((hand, index) => {
+            let multiplier = index + 1
+            let bet = this.hands.get(hand) || 0
+            sum += (multiplier * bet)
+            //console.log(hand, bet, multiplier, typeofwin(hand))
+        });
+        return sum;
+    }
+}
